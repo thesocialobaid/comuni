dcfbcbb0d758e0011f2882eee4704346dac09376
Maida-Adnan <maidaadnan203@gmail.com>
2026-03-08 22:24:16 +0500

    Add validation fixes

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,10 @@
 -- 24L-0698,24L-0650,24L-0509
 DROP DATABASE IF EXISTS communitask;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 CREATE DATABASE IF NOT EXISTS communitask;
